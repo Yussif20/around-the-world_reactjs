7e47009e944d989dd2d4e922b79b49223c30b142
Yousef ayman <yousefayman2000500@gmail.com>
2024-10-11 06:58:05 +0300

    add the search bar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from './components/Header';
+import SearchBar from './components/SearchBar';
 
 function App() {
   return (
     <div className="font-inter  dark:text-[#F3F4F6]">
       <Header />
+      <SearchBar />
     </div>
   );
 }
