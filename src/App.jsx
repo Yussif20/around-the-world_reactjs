802d48ba6a9a32b3eabca163abd57ace64921a0d
Yousef ayman <yousefayman2000500@gmail.com>
2024-10-11 21:33:55 +0300

    filter data by region

--- src/App.jsx
+++ src/App.jsx
@@ -3,31 +3,47 @@ import CountryList from './components/CountryList';
 import Header from './components/Header';
 import RegionMenu from './components/RegionMenu';
 import SearchBar from './components/SearchBar';
+import ErrorMessage from './components/ErrorMessage';
 
 function App() {
   const [countriesList, setCountriesList] = useState([]);
+  const [filteredCountries, setFilteredCountries] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
   useEffect(() => {
     fetchCountryData();
   }, []);
 
   const fetchCountryData = () => {
-    fetch('https://restcountries.com/v3.1/all').then((res) =>
-      res.json().then((data) => {
-        console.log(data);
-        setCountriesList(data);
-      })
-    );
+    fetch('https://restcountries.com/v3.1/all')
+      .then((res) =>
+        res.json().then((data) => {
+          setCountriesList(data);
+          setFilteredCountries(data);
+        })
+      )
+      .catch(() => setIsError(true))
+      .finally(() => setIsLoading(false));
   };
   return (
     <div className="min-h-screen w-screen bg-gray-100 font-inter dark:bg-gray-900 dark:text-gray-100">
       <Header />
-      <div className="container mx-auto px-5 md:px-8">
-        <div className="flex flex-col justify-between gap-10 md:h-14 md:flex-row md:gap-0">
-          <SearchBar />
-          <RegionMenu />
-        </div>
-      </div>
-      <CountryList countries={countriesList} />
+      {isLoading && <ErrorMessage message={'Data is Loading...'} />}
+      {isError && <ErrorMessage message={'Something went wrong...'} />}
+      {!isError && !isLoading && (
+        <>
+          <div className="container mx-auto px-5 md:px-8">
+            <div className="flex flex-col justify-between gap-10 md:h-14 md:flex-row md:gap-0">
+              <SearchBar />
+              <RegionMenu
+                countryList={countriesList}
+                setFilteredCountries={setFilteredCountries}
+              />
+            </div>
+          </div>
+          <CountryList countries={filteredCountries} />
+        </>
+      )}
     </div>
   );
 }
--- src/components/RegionMenu.jsx
+++ src/components/RegionMenu.jsx
@@ -1,18 +1,33 @@
+/* eslint-disable react/prop-types */
 import Select from 'react-select';
 
 const options = [
-  { value: 'chocolate', label: 'Chocolate' },
-  { value: 'strawberry', label: 'Strawberry' },
-  { value: 'vanilla', label: 'Vanilla' },
+  { value: 'all regions', label: 'All regions' },
+  { value: 'africa', label: 'Africa' },
+  { value: 'asia', label: 'Asia' },
+  { value: 'europe', label: 'Europe' },
+  { value: 'oceania', label: 'Oceania' },
+  { value: 'americas', label: 'Americas' },
+  { value: 'antarctic', label: 'Antarctic' },
 ];
 
-const RegionMenu = () => {
+const RegionMenu = ({ countryList, setFilteredCountries }) => {
+  const regionChangeHandler = (e) => {
+    const region = e.label;
+    const filteredList =
+      region === 'All regions'
+        ? countryList
+        : countryList.filter((country) => country.region === region);
+    setFilteredCountries(filteredList);
+  };
   return (
     <Select
+      defaultValue={options[0]}
+      onChange={regionChangeHandler}
       classNames={{
         indicatorSeparator: () => 'hidden',
         input: () => 'dark:text-gray-100',
-        singleValue: () => 'dark:text-gray-100',
+        singleValue: () => 'dark:text-gray-800',
         option: () => 'hover:!text-gray-800',
         menu: () =>
           'bg-gray-100 text-gray-800 dark:bg-gray-800 dark:text-gray-100',
